3f8319178af77ebf132a95005238c1a87fd58479
BRYN <bryn4444@gmail.com>
2025-12-07 11:06:04 -0700

    No Pacifications Complication will now only activate if both the Specific Disguises Kill Requirement and Force Difficulty Gameplay Challenge are set to Off.

--- js/Code.js
+++ js/Code.js
@@ -78,7 +78,7 @@ function createExtrasList(exit, missionCode) {
 	if (Math.random() < 0.20 && document.getElementById("rating").checked == 0)
 		extras.push("Do Not Get Spotted|Avoid anyone seeing you performing any illegal action.");
 
-	if (Math.random() < 0.30 && disguise == "OFF" && !koExits.includes(exit) && document.getElementById("exobj").checked == 0)
+	if (Math.random() < 0.30 && disguise == "OFF" && document.getElementById("difficulty").selectedIndex == 0 && !koExits.includes(exit) && document.getElementById("exobj").checked == 0)
 		extras.push("No Pacifications|Do not pacify or subdue anyone in any way. This includes using items via melee/throwing, sedating, or tranquilizing.");
 
 	if (Math.random() < 0.25)
